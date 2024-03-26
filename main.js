6b52dea9564cf1a362b1fba91baf0c51389ac430
HamburgerWithExtraMayo <150495029+HamburgerWithExtraMayo@users.noreply.github.com>
2024-03-27 00:26:37 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamburgerMenu = document.querySelector(".hamborgir");
+const rightNav = document.querySelector(".right-nav");
+const navbar = document.querySelector(".navbar");
+const all = document.querySelector("*");
+
+hamburgerMenu.addEventListener("click", () => {
+    rightNav.classList.toggle("right-nav-toggle");
+
+    
+});
